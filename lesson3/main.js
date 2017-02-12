b9b17919550d60d0426cb6a72af3dc11cc7cb9c1
praneethkumarpidugu <pidugu2007@gmail.com>
2017-02-12 18:10:11 -0500

    added data binding to dom

--- lesson3/main.js
+++ lesson3/main.js
@@ -2,4 +2,16 @@
 //Application name for module.
 var app = angular.module('minmax', []);
 
+app.controller('MinMaxCtrl', function($scope) {
+    "use strict";
+    //We will use this formModel to bind to the view
+    $scope.formModel = {};
+
+    $scope.onSubmit = function () {
+      console.log("Hey I'm submitted");
+      console.log($scope.formModel);
+    };
+});
 // https://minmax-server.herokuapp.com/register/'
+
+//Define our controller to associate to our DOM element
