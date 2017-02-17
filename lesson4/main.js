3eff8c1157ea00f1dd2d3809fa4bdc2ef91226df
praneethkumarpidugu <pidugu2007@gmail.com>
2017-02-16 23:28:59 -0500

    Form Validation using HTML5:- Added the boiler plate code from lesson 3.

--- lesson4/main.js
+++ lesson4/main.js
@@ -1,18 +1,21 @@
+/**
+ * Created by praneethkumar on 16/02/17.
+ */
+
 var app = angular.module('minmax', []);
 
-
 app.controller('MinMaxCtrl', function ($scope, $http) {
-	$scope.formModel = {};
+   $scope.formModel = {};
 
-	$scope.onSubmit = function () {
-		console.log("Hey i'm submitted!");
-		console.log($scope.formModel);
+   //Creating a function called onSubmit for Register button
+    $scope.onSubmit = function () {
+      console.log("I'm submitted");
+      console.log($scope.formModel);
 
-		$http.post('https://minmax-server.herokuapp.com/register/', $scope.formModel).
-			success(function (data) {
-				console.log(":)")
-			}).error(function(data) {
-				console.log(":(")
-			});
-	};
+      $http.post('https://minmax-server.herokuapp.com/register/', $scope.formModel).success(function (data) {
+          console.log(":)");
+      }).error(function (data) {
+          console.log(":(")
+      });
+    };
 });
