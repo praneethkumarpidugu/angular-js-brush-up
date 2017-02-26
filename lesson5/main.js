80eb3142974ac08a14544c2b762fb0d37baeda75
praneethkumarpidugu <pidugu2007@gmail.com>
2017-02-26 15:39:56 -0500

    [Form Validation using Angular.js]: Added Boilerplate for this lesson

--- lesson5/main.js
+++ lesson5/main.js
@@ -1,18 +1,25 @@
+/**
+ * Created by praneethkumar on 26/02/17.
+ */
+
 var app = angular.module('minmax', []);
 
-
 app.controller('MinMaxCtrl', function ($scope, $http) {
-	$scope.formModel = {};
 
-	$scope.onSubmit = function () {
-		console.log("Hey i'm submitted!");
-		console.log($scope.formModel);
+    $scope.formModel = {};
 
-		$http.post('https://minmax-server.herokuapp.com/register/', $scope.formModel).
-			success(function (data) {
-				console.log(":)")
-			}).error(function(data) {
-				console.log(":(")
-			});
-	};
+    //Create a function onSubmit for Register Button
+
+    $scope.onSubmit = function () {
+        console.log("I'm submitted");
+        console.log($scope.formModel);
+
+        $http.post('https://minmax-server.herokuapp.com/register/', $scope.formModel).
+            success(function (data) {
+            console.log(":)");
+        }).error(function (data) {
+            console.log(":(");
+        });
+    };
+
 });
