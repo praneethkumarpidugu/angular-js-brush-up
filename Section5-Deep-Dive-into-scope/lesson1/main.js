0dbf38c080ee4f99bea86a4b9ebd0f1e863a0702
praneethkumarpidugu <pidugu2007@gmail.com>
2017-04-15 16:04:22 -0400

    [Deep dive into Scope]

---
+++ Section5-Deep-Dive-into-scope/lesson1/main.js
@@ -0,0 +1,16 @@
+/**
+ * Created by praneethkumar on 15/04/17.
+ */
+
+var app = angular.module('codecraft', []);
+
+app.controller('ParentController', function ($scope) {
+    $scope.name = "Parent";
+
+    $scope.reset = function () {
+        $scope.name = "Parent";
+    };
+});
+app.controller('ChildController', function () {
+
+})
